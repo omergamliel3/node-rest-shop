94508d238aef8c1b396341cb68d0e4cf78e965f1
Omer Gamliel <omergamliel3@gmail.com>
2020-12-17 14:37:18 +0200

    handle incoming POST requests

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,8 +10,13 @@ router.get('/', (req, res, next) => {
 
 // Handle incoming POST requests to /orders
 router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
     res.status(201).json({
-        message: 'Order was created'
+        message: 'Order was created',
+        order: order
     });
 });
 
