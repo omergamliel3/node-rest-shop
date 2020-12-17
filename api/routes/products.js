072a8ab1180219e7638298bdff6762a24f99e9cd
Omer Gamliel <omergamliel3@gmail.com>
2020-12-17 14:37:23 +0200

    handle incoming POST requests

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,8 +10,14 @@ router.get('/', (req, res, next) => {
 
 // Handle incoming POST requests to /products
 router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+
     res.status(201).json({
-        message: 'Handling POST requests to /products'
+        message: 'Handling POST requests to /products',
+        createdProduct: product
     })
 });
 
