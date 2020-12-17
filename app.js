551c4801d7da4360765c0f6e46a02325d5f3d4d0
Omer Gamliel <omergamliel3@gmail.com>
2020-12-17 14:22:35 +0200

    add bodyParser middleware

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express();
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 const productsRoutes = require('./api/routes/products')
 const ordersRoutes = require('./api/routes/orders')
 
 // Setup morgan middleware
 app.use(morgan('dev'));
+
+// Setup bodyParser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // Routes which should handle requests
 app.use('/products', productsRoutes);
