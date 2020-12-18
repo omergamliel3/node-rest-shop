4503eee1d12657418c5716a453486f617c40ae00
Omer Gamliel <omergamliel3@gmail.com>
2020-12-18 13:41:15 +0200

    save product in mongoDB when handle post request

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router();
+const mongoose = require('mongoose')
+
+const Product = require('../models/product')
+
 
 // Handle incoming GET requests to /products
 router.get('/', (req, res, next) => {
@@ -10,11 +14,23 @@ router.get('/', (req, res, next) => {
 
 // Handle incoming POST requests to /products
 router.post('/', (req, res, next) => {
-    const product = {
+    // Create new Product instance
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-
+    });
+    // Saves this document by inserting a new document into the database
+    product
+        .save()
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    
+    // Return response
     res.status(201).json({
         message: 'Handling POST requests to /products',
         createdProduct: product
