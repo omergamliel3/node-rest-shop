1c15ebec9f14bc718b814f51f454e02c55c5b1b8
Omer Gamliel <omergamliel3@gmail.com>
2020-12-18 13:40:36 +0200

    correct product schema id type

--- api/models/product.js
+++ api/models/product.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 // Create product mongoDB schema
 const productSchema = mongoose.Schema({
-        _id: mongoose.Types.ObjectId,
+        _id: mongoose.Schema.Types.ObjectId,
         name: String,
         price: Number
     });
