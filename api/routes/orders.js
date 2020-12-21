cdd657ab815c866248031f8a3481f1b1efb1a918
Omer Gamliel <omergamliel3@gmail.com>
2020-12-21 16:02:20 +0200

    convert promises to async & await

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -6,105 +6,107 @@ const Order = require("../models/order");
 const Product = require("../models/product");
 
 // Handle incoming GET requests to /orders
-router.get("/", (req, res, next) => {
-  // Get all orders from the database
-  Order.find()
-    .select("product quantity _id")
-    .populate("product", "_id name price")
-    .exec()
-    .then((docs) => {
-      const response = {
-        count: docs.length,
-        orders: docs.map((doc) => {
-          return {
-            id: doc._id,
-            product: doc.product,
-            quantity: doc.quantity,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/orders/" + doc._id,
-            },
-          };
-        }),
-      };
-      res.status(200).json(response);
-    })
-    .catch((error) => {
-      res.status(500).json({
-        error: error,
-      });
+router.get("/", async (req, res, next) => {
+  try {
+    // Find order from the database
+    const docs = await Order.find()
+      .select("product quantity _id")
+      .populate("product", "_id name price");
+
+    // Deconstruct reponse
+    const response = {
+      count: docs.length,
+      orders: docs.map((doc) => {
+        return {
+          id: doc._id,
+          product: doc.product,
+          quantity: doc.quantity,
+        };
+      }),
+    };
+    // Success response
+    res.status(200).json(response);
+  } catch (error) {
+    // Error response
+    res.status(500).json({
+      error: error,
     });
+  }
 });
 
 // Handle incoming POST requests to /orders
-router.post("/", (req, res, next) => {
-  // Find product by id
-  Product.findById(req.body.productId)
-    .exec()
-    .then((product) => {
-      if (!product) {
-        return res.status(404).json({
-          message: "Product not found",
-        });
-      }
-      // Create a new Order instance
-      const order = new Order({
-        _id: new mongoose.Types.ObjectId(),
-        product: req.body.productId,
-        quantity: req.body.quantity,
+router.post("/", async (req, res, next) => {
+  try {
+    // Find product by id
+    const product = await Product.findById(req.body.productId);
+
+    // Return error response if no product found in database
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
       });
-      // Insert new Order to the database
-      return order.save();
-    })
-    .then((doc) => {
-      const response = {
-        message: "Created order successfully",
-      };
-      res.status(200).json(response);
-    })
-    .catch((error) => {
-      res.status(500).json({
-        error: error,
-      });
+    }
+    // Create a new Order instance
+    const order = new Order({
+      _id: new mongoose.Types.ObjectId(),
+      product: req.body.productId,
+      quantity: req.body.quantity,
     });
+
+    // Insert order into the database
+    await order.save();
+    const response = {
+      message: "Created order successfully",
+    };
+    // Return success response
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({
+      error: error,
+    });
+  }
 });
 
 // Handle incoming GET requests to /orders/orderId
-router.get("/:orderId", (req, res, next) => {
-  Order.findById(req.params.orderId)
-    .populate("product", "_id name price")
-    .exec()
-    .then((doc) => {
-      const response = {
-        id: doc._id,
-        product: doc.product,
-        quantity: doc.quantity,
-      };
-      res.status(200).json(response);
-    })
-    .catch((error) => {
-      res.status(500).json({
-        error: error,
-      });
+router.get("/:orderId", async (req, res, next) => {
+  try {
+    // Find order by id
+    const doc = await Order.findById(req.params.orderId).populate(
+      "product",
+      "_id name price"
+    );
+
+    // Deconstruct reponse
+    const response = {
+      id: doc._id,
+      product: doc.product,
+      quantity: doc.quantity,
+    };
+    // Success response
+    res.status(200).json(response);
+  } catch (error) {
+    // Error response
+    res.status(500).json({
+      error: error,
     });
+  }
 });
 
 // Handle incoming DELETE requests to /orders/:orderId
-router.delete("/:orderId", (req, res, next) => {
-  // Find order with the given id
-  Order.remove({ _id: req.params.orderId })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "Order deleted",
-      });
-    })
-    .catch((error) => {
-      res.status(404).json({
-        message: "No valid entry found for provided ID",
-        error: error,
-      });
+router.delete("/:orderId", async (req, res, next) => {
+  try {
+    // Find order by id
+    const result = await Order.remove({ _id: req.params.orderId });
+    // Success response
+    res.status(200).json({
+      message: "Order deleted",
     });
+  } catch (error) {
+    // Error response
+    res.status(404).json({
+      message: "No valid entry found for provided ID",
+    });
+  }
 });
 
 module.exports = router;
